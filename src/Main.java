3bb4fed50a042e962d910745f71fc684a22fa3fa
renikQ <trog94@mail.ru>
2022-08-20 03:11:03 +0500

    Completed homework

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
         // Задание 1
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
+
         // Задание 2
         String fullNameForReport = fullName.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameForReport);
+
         // Задание 3
         fullName = "Иванов Семён Семёнович";
         String newFullName = fullName.replace('ё', 'е');
